a058fd0f57bfd93c44ac502d7bceb37cde9a6397
Brian Rosner <brosner@gmail.com>
2008-11-04 19:44:26 -0700

    Cleaned up the indentation in C files.

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 void kmain()
 {
-  clrscr();
-
-  char banner[] = {'C', 'l', 'e', 'e', 's', 'e', ' ', '2', '0', '0', '8', '\0'};
-  print(banner);
-
-  for(;;);
+    clrscr();
+    
+    char banner[] = {'C', 'l', 'e', 'e', 's', 'e', ' ', '2', '0', '0', '8', '\0'};
+    print(banner);
+    
+    for(;;);
 }
--- video.c
+++ video.c
@@ -1,47 +1,47 @@
 void clrscr()
 {
-  unsigned char *vidmem = (unsigned char *)0xB8000;
-  const long size = 80*25;
-  long loop;
-
-  // Clear visible video memory
-  for (loop=0; loop<size; loop++) {
-    *vidmem++ = 0;
-    *vidmem++ = 0x0F;
-  }
-
-  // Set cursor position to 0,0
-  out(0x3D4, 14);
-  out(0x3D5, 0);
-  out(0x3D4, 15);
-  out(0x3D5, 0);
+    unsigned char *vidmem = (unsigned char *) 0xB8000;
+    const long size = 80 * 25;
+    long loop;
+    
+    // Clear visible video memory
+    for (loop=0; loop<size; loop++) {
+        *vidmem++ = 0;
+        *vidmem++ = 0x0F;
+    }
+    
+    // Set cursor position to 0,0
+    out(0x3D4, 14);
+    out(0x3D5, 0);
+    out(0x3D4, 15);
+    out(0x3D5, 0);
 }
 
 void print(const char *_message)
 {
-  unsigned short offset;
-  unsigned long i;
-  unsigned char *vidmem = (unsigned char *)0xB8000;
-
-  // Read cursor position
-  out(0x3D4, 14);
-  offset = in(0x3D5) << 8;
-  out(0x3D4, 15);
-  offset |= in(0x3D5);
-
-  // Start at writing at cursor position
-  vidmem += offset*2;
-
-  // Continue until we reach null character
-  i = 0;
-  while (_message[i] != 0) {
-    *vidmem = _message[i++];
-	vidmem += 2;
-  }
-
-  // Set new cursor position
-  offset += i;
-  out(0x3D5, (unsigned char)(offset));
-  out(0x3D4, 14);
-  out(0x3D5, (unsigned char)(offset >> 8));
+    unsigned short offset;
+    unsigned long i;
+    unsigned char *vidmem = (unsigned char *)0xB8000;
+    
+    // Read cursor position
+    out(0x3D4, 14);
+    offset = in(0x3D5) << 8;
+    out(0x3D4, 15);
+    offset |= in(0x3D5);
+    
+    // Start at writing at cursor position
+    vidmem += offset*2;
+    
+    // Continue until we reach null character
+    i = 0;
+    while (_message[i] != 0) {
+        *vidmem = _message[i++];
+        vidmem += 2;
+    }
+    
+    // Set new cursor position
+    offset += i;
+    out(0x3D5, (unsigned char)(offset));
+    out(0x3D4, 14);
+    out(0x3D5, (unsigned char)(offset >> 8));
 }
