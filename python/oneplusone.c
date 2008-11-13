d19a1faef0b44684eccb3230fd5c3d0e9ae408ea
James Tauber <jtauber@jtauber.com>
2008-11-13 01:37:44 -0500

    got as far as PyMarshal_ReadObjectFromString failing -- note the frozen module is from Cleese 2003 and won't ultimately work

--- python/oneplusone.c
+++ python/oneplusone.c
@@ -7,9 +7,18 @@ and calls FrozenMain after setting up own frozen module(s)
 
 #include "Python.h"
 
+unsigned char x[] = {
+        99,0,0,0,0,0,0,0,0,2,0,0,0,64,0,0,
+        0,115,13,0,0,0,100,0,0,100,0,0,23,71,72,100,
+        1,0,83,40,2,0,0,0,105,1,0,0,0,78,40,0,
+        0,0,0,40,0,0,0,0,40,0,0,0,0,40,0,0,
+        0,0,115,7,0,0,0,116,101,115,116,46,112,121,115,1,
+        0,0,0,63,1,0,0,0,115,0,0,0,0,
+};
+
 static struct _frozen frozenModules[] = {
-//        {"__main__", <bytes>, <len>}, 
-//        {0, 0, 0} /* sentinel */
+        {"__main__", x, 93}, 
+	{0, 0, 0} /* sentinel */
 };
 
 struct _frozen *PyImport_FrozenModules = frozenModules;
